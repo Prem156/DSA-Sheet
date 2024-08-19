21467eab721b388cd1c989bb44820dacccb13aed
Prem <vermaprem.pm@gmail.com>
2024-08-19 16:42:32 +0530

    Update in Neither Max not Min

--- Arrays/Medium/NeitherMinNorMax.java
+++ Arrays/Medium/NeitherMinNorMax.java
@@ -1,4 +1,19 @@
 public class NeitherMinNorMax {
+
+    public static int findNonMinOrmax(int[] nums) {
+        if(nums.length==2 || nums.length==1)return -1;
+        if(nums[0]<nums[1] && nums[1]<nums[2] || nums[0]>nums[1] && nums[1]>nums[2])
+        {
+            return nums[1];
+        }
+        else if(nums[1]<nums[0] && nums[0]<nums[2] || nums[0]<nums[1] && nums[0]>nums[2])
+        {
+            return nums[0];
+        }
+
+        return nums[2];
+    }
+
         static int findNonMinOrMax(int[] nums) {
             if(nums.length <= 2) {
                 return -1;
